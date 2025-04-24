cfd1e614cb8cbe3de2dddd166d1e102cf8503d02
Master <ducdeptraivodich23@gmail.com>
2025-04-24 10:33:21 +0700

    update proxy getChapterImage

--- src/api/Manga/getChapterImages.tsx
+++ src/api/Manga/getChapterImages.tsx
@@ -1,5 +1,12 @@
+// src/api/Manga/getChapterImages.ts
 export const getChapterImages = async (chapterId: string) => {
-  const res = await fetch(`https://api.mangadex.org/at-home/server/${chapterId}`)
-  if (!res.ok) throw new Error('Failed to fetch chapter images')
+  const res = await fetch(`/api/proxy?endpoint=at-home/server/${chapterId}`, {
+    next: { revalidate: 60 }
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch chapter images')
+  }
+
   return res.json()
 }
